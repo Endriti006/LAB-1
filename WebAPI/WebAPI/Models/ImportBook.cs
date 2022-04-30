541014b8fab53b66004d32d6698cd67e01429a2f
artshabani <ardiosmani02@gmail.com>
2022-04-30 17:12:09 +0200

    WebAPI - .Net

---
+++ WebAPI/WebAPI/Models/ImportBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class ImportBook
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string BookName { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string Sasia { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string authorName { get; set; }
+
+        public int DataBotimit { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string Zhanra { get; set; }
+
+        
+    }
+}
